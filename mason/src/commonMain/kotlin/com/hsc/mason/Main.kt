1382358f947d0b977100a922737af0892cd4938e
Sandy <70289658+sndyx@users.noreply.github.com>
2024-03-27 01:29:43 -0400

    Add `mason init` command

--- mason/src/commonMain/kotlin/com/hsc/mason/Main.kt
+++ mason/src/commonMain/kotlin/com/hsc/mason/Main.kt
@@ -3,7 +3,8 @@ package com.hsc.mason
 import com.github.ajalt.clikt.core.subcommands
 import com.hsc.mason.commands.BuildCommand
 import com.hsc.mason.commands.CleanCommand
+import com.hsc.mason.commands.InitCommand
 import com.hsc.mason.commands.MasonCommand
 
 fun main(args: Array<String>) = MasonCommand()
-    .subcommands(BuildCommand(), CleanCommand()).main(args)
+    .subcommands(BuildCommand(), CleanCommand(), InitCommand()).main(args)
---
+++ mason/src/commonMain/kotlin/com/hsc/mason/commands/InitCommand.kt
@@ -0,0 +1,44 @@
+package com.hsc.mason.commands
+
+import com.github.ajalt.clikt.core.CliktCommand
+import com.github.ajalt.clikt.parameters.arguments.argument
+import com.github.ajalt.clikt.parameters.arguments.optional
+import okio.FileSystem
+import okio.Path
+import okio.Path.Companion.toPath
+import okio.SYSTEM
+
+class InitCommand : CliktCommand() {
+
+    private val _path: String? by argument().optional()
+    private lateinit var path: Path
+
+    private val fs = FileSystem.SYSTEM
+
+    override fun run() {
+        path = _path?.toPath() ?: "".toPath()
+
+        if (!fs.exists(path.resolve("House.toml"))) {
+
+            fs.write(path.resolve("House.toml")) {
+                writeUtf8("""
+                # see https://github.com/sndyx/hsl/blob/master/docs/reference/manifest.md for reference
+                [package]
+                name = "example_house"
+                version = "1.0.0"
+                """.trimIndent())
+            }
+
+            fs.createDirectory(path.resolve("src"))
+            fs.write(path.resolve("src/example.hsl")) {
+                writeUtf8("""
+                fn example_function() {
+                    send_message("Hello from HSL!")
+                }
+                """.trimIndent())
+            }
+
+        }
+    }
+
+}
